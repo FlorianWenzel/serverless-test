4cbdf269fc753252ad23e06ef9259ff99b4ed819
FlorianWenzel <github@florian-wenzel.io>
2020-04-22 17:53:42 +0200

    test

--- api/test.js
+++ api/test.js
@@ -1,9 +1,7 @@
-const serverless = require('serverless-http');
-const express = require('express')
-const app = express()
-
-app.get('/test', function(req, res) {
-  res.send('Hello World!')
-})
-
-module.exports.handler = serverless(app);
+module.exports = (req, res) => {
+  res.json({
+    body: req.body,
+    query: req.query,
+    cookies: req.cookies
+  })
+}

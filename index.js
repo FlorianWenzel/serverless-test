a89b6917f746826d24578044b28195be93418be2
FlorianWenzel <github@florian-wenzel.io>
2020-04-22 18:18:35 +0200

    test

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const app = require("express")();
 const http = require("http").createServer(app);
 
 app.get("*", async (req, res) => {
+  console.log('this')
   res.sendFile(__dirname + "/dist/index.html");
 });
 

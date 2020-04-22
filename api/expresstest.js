fb677842be6b1f814e8242197b869548cf4b03f3
FlorianWenzel <github@florian-wenzel.io>
2020-04-22 18:47:37 +0200

    test

--- api/expresstest.js
+++ api/expresstest.js
@@ -3,6 +3,5 @@ const fs = require("fs");
 const multerConfig = require("../multer.js");
 
 module.exports = app.post("/api/expresstest", multerConfig.saveToUploads, async (req, res) => {
-  const file = fs.readFileSync("/tmp/whatevs.jpg")
   res.send({ sucess: true });
 })
